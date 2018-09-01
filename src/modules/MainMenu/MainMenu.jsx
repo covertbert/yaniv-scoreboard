c8f808959929f537f8a6e4038c06992cb0b999bb
Bertie Blackman <blackmanrgh@gmail.com>
2018-09-01 20:43:34 +0100

    Styles main menu screen

--- src/modules/MainMenu/MainMenu.jsx
+++ src/modules/MainMenu/MainMenu.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-
-import { Link } from 'react-router-dom'
+import { Box, Button } from 'gestalt'
+import CenterWrapper from '../../components/CenterWrapper'
+import PlayerManagementButton from '../../components/PlayerManagementButton'
+// import { Link } from 'react-router-dom'
 
 const MainMenu = () => (
-  <ul>
-    <li>
-      <Link to="/player-management">
-        Player Management
-      </Link>
-    </li>
-  </ul>
+  <CenterWrapper>
+    <PlayerManagementButton />
+    <Box width="100%">
+      <Box padding={2}>
+        <Button accessibilityLabel="New Game" text="New game" color="blue" />
+      </Box>
+      <Box padding={2}>
+        <Button accessibilityLabel="Load Game" text="Load game" />
+      </Box>
+    </Box>
+  </CenterWrapper>
 )
 
 export default MainMenu
