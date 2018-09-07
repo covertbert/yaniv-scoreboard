7f3a34fc6bbc6494ede651ecbe73f3f102e301ba
Bertie Blackman <blackmanrgh@gmail.com>
2018-09-07 16:15:38 +0100

    Adds router tests

--- src/routes/__tests__/Routes.test.jsx
+++ src/routes/__tests__/Routes.test.jsx
@@ -18,11 +18,27 @@ const renderWithRouter = (
 
 describe('Routes', () => {
   it('loads the index page', () => {
-    const { container, getByTestId } = renderWithRouter(<Routes />)
-    expect(container.innerHTML).toMatch('New Game')
+    const { container } = renderWithRouter(<Routes />)
+
+    expect(container.innerHTML).toMatch('New game')
+    expect(container.innerHTML).toMatch('Load game')
+  })
+
+  it('loads the New Game page when link is clicked', () => {
+    const { container, getByText } = renderWithRouter(<Routes />)
+
     const leftClick = { button: 0 }
-    fireEvent.click(getByTestId('AppHeader/PlayerManagementButton'), leftClick)
-    // // normally I'd use a data-testid, but just wanted to show this is also possible
-    // expect(container.innerHTML).toMatch('You are on the about page')
+    fireEvent.click(getByText('New game'), leftClick)
+
+    expect(container.innerHTML).toMatch('New Game')
+  })
+
+  it('loads the Load Game page when link is clicked', () => {
+    const { container, getByText } = renderWithRouter(<Routes />)
+
+    const leftClick = { button: 0 }
+    fireEvent.click(getByText('Load game'), leftClick)
+
+    expect(container.innerHTML).toMatch('Load Game')
   })
 })
