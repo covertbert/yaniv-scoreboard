917a1e515d900539cd084fe4ee4371acfbcdc42a
Bertie Blackman <blackmanrgh@gmail.com>
2018-09-12 21:49:41 +0100

    Adds some weak container tests

--- src/modules/PlayerManagement/NewPlayerButton/__tests__/NewPlayerButtonContainer.test.js
+++ src/modules/PlayerManagement/NewPlayerButton/__tests__/NewPlayerButtonContainer.test.js
@@ -1,12 +1,7 @@
 import { createHandlers } from '../NewPlayerButtonContainer'
 
 describe('createPlayerMutation', () => {
-  test('calls the mutate function', async () => {
-    const mutate = jest.fn()
-    const refetchQuery = 'test'
-
-    await createHandlers.createPlayerMutation({ mutate, refetchQuery })('Bill')
-
-    expect(mutate).toBeCalled()
+  test('is a function', async () => {
+    expect(await createHandlers.createPlayerMutation('Bill')).toBeInstanceOf(Function)
   })
 })
--- src/modules/PlayerManagement/PlayerCard/__tests__/PlayerCardContainer.test.js
+++ src/modules/PlayerManagement/PlayerCard/__tests__/PlayerCardContainer.test.js
@@ -1,13 +1,7 @@
 import { createHandlers } from '../PlayerCardContainer'
 
-describe('createPlayerMutation', () => {
-  test('calls the mutate function', async () => {
-    expect(2 > 1)
-    // const mutate = jest.fn()
-    // const refetchQuery = 'test'
-
-    // await createHandlers.deletePlayerMutation({ mutate, refetchQuery })('Bill')
-
-    // // expect(hhh).toEqual()
+describe('deletePlayerMutation', () => {
+  test('is a function', async () => {
+    expect(await createHandlers.deletePlayerMutation('Bill')).toBeInstanceOf(Function)
   })
 })
